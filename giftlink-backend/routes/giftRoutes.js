895c2f01cddfbe5c0aeaec1ababf068624bc6c75
nanneisan <khinmaykyaw317165@gmail.com>
2025-11-19 03:10:28 -0500

    add ; in app.js and gift routes

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,5 +1,5 @@
-const connectToDatabase = require('../models/db')
-const express = require('express')
+const connectToDatabase = require('../models/db');
+const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
